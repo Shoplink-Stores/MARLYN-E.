c08fc13bccf07029119b27fc3beb6a75b03c9167
Shoplink-Stores <d87683326@gmail.com>
2026-07-24 23:13:49 +0800

    Create products.js

---
+++ UAE/products.js
@@ -0,0 +1,41 @@
+const products = {
+
+    product1: {
+
+        QA: "https://shop.jcpremiere.com/products/e1f2d5714d6048d6a93ea28f387195bf/11111510551"
+        
+        PH: "https://shop.jcpremiere.com/products/5877e5b1d21e4d5db3cec55b3c5544f7/11111510551",
+
+        SG: "https://shop.jcpremiere.com/products/e1f2d5714d6048d6a93ea28f387195bf/11111510551",
+
+        TW: "https://shop.jcpremiere.com/products/e1f2d5714d6048d6a93ea28f387195bf/11111510551",
+
+        JP: "https://shop.jcpremiere.com/products/e1f2d5714d6048d6a93ea28f387195bf/11111510551"
+
+    },
+
+    product2: {
+
+        PH: "https://shop.jcpremiere.com/products/xxxxxxxx",
+
+        SG: "https://shop.jcpremiere.com/products/yyyyyyyy",
+
+        TW: "https://shop.jcpremiere.com/products/zzzzzzzz",
+
+        JP: "https://shop.jcpremiere.com/products/aaaaaaaa"
+
+    },
+
+    product3: {
+
+        PH: "...",
+
+        SG: "...",
+
+        TW: "...",
+
+        JP: "..."
+
+    }
+
+};
