47cec20aa6c3518ec54384f56df355a2d2db7fd0
Shoplink-Stores <d87683326@gmail.com>
2026-07-25 03:45:02 +0800

    Update products.js

--- UAE/products.js
+++ UAE/products.js
@@ -1,8 +1,6 @@
 const products = {
 
     product1: {
-
-        QA: "https://shop.jcpremiere.com/products/e1f2d5714d6048d6a93ea28f387195bf/11111510551",
         
         PH: "https://shop.jcpremiere.com/products/5877e5b1d21e4d5db3cec55b3c5544f7/11111510551",
 
