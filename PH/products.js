3ab3ee4e427fa3c12f579e971d55041392072c09
Shoplink-Stores <d87683326@gmail.com>
2026-07-26 11:18:05 +0800

    Update products.js

--- PH/products.js
+++ PH/products.js
@@ -1,5 +1,11 @@
 const products = {
 
+    country: {
+
+        PH:"https://Shoplink-Stores/MARLYN Es/PH/shop.html",
+
+        AE:"",
+
     product1: {
         
         PH: "https://shop.jcpremiere.com/products/5877e5b1d21e4d5db3cec55b3c5544f7/11111510551",
